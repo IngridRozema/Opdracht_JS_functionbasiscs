e6e850af2d87e18167a5e4c3e50eb0bb65158dbb
Ingrid Rozema <67099368+IngridRozema@users.noreply.github.com>
2021-07-08 12:52:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const makePizza = function (){
+    console.log("deeg maken");
+    console.log("pizzadeeg uitrollen tot ronde bodem")
+    console.log("Bodem besmeren met tomatensaus")
+    console.log("topping van jouw keuze toevoegen")
+    console.log("bak de pizza in de oven")
+};
+
+const makeSushi = function (){
+    console.log("kook de rijst.")
+    console.log("Leg de sushimatjes klaar en leg de nourivellen erop.")
+    console.log("Verdeel de afgekoelde rijst over de nourivellen.")
+    console.log("Voeg de ingrediënten van jouw keuze toe en maak een mooi rolletje")
+    console.log("Eet smakelijk!")
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
